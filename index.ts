dd92dd25f279cadc3cf4ff7332d654d0f25b57ed
Anwesh43 <anweshthecool0@gmail.com>
2019-10-06 16:07:36 +0530

    adding BoomerangBlock class

--- index.ts
+++ index.ts
@@ -171,3 +171,27 @@ class BBNode {
         return this
     }
 }
+
+class BoomerangBlock {
+
+    root : BBNode = new BBNode(0)
+    curr : BBNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
