333889de2d4390a348095afb42841ebf5aad34f8
Anwesh43 <anweshthecool0@gmail.com>
2019-10-06 15:30:50 +0530

    adding Animator class

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ const sizeFactor : number = 2.9
 const nodes : number = 5
 const foreColor : string = "#4CAF50"
 const backColor : string = "#BDBDBD"
+const delay : number = 30
 
 class BoomerangBlockStage {
 
@@ -58,6 +59,26 @@ class State {
         if (this.dir == 0) {
             this.dir = 1 - 2 * this.prevScale
             cb()
+        }
+    }
+}
+
+class Animator {
+
+    animated : boolean = false
+    interval : number
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
         }
     }
 }
